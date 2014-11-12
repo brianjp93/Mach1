3cab3cb48c08f0336b1e9c27108e111f1b775d1f
Brianjp93 <brianjp93@gmail.com>
2014-11-11 22:22:55 -0800

    Additions

--- mach1.py
+++ mach1.py
@@ -7,35 +7,65 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Mach1():
-
 	# static variables
-	cmd = {"home": 0, "moveAbsolute": 0, "moveRelative": 0}
+	cmd = {
+	"home": 1, 
+	"moveAbsolute": 20, 
+	"moveRelative": 21,
+	"setTargetSpeed": 42,
+	"storeCurrentPosition": 16
+	}
+
 	translation = {"hor": 0, "ver": 1}
+	# microstep in mm
+	microstep = 0.00009921875
 
-	def __init__():
+	def __init__(self):
 		# Serial() input depends on where stage is connected
-		stage = serial.Serial(2)
-		osc = TDS3k(serial.Serial("COM1", 9600, timeout=1))
+		self.stage = serial.Serial(2)
+		self.osc = TDS3k(serial.Serial("COM1", 9600, timeout=1))
 
-	def convertSpeed(v):
+	def convertSpeed(self, v):
 		"""
-		input
+		v - in mm/s
+			converts v to units that make sense to stage and returns converted
 		"""
-		converted = 0
+		converted = 1/(Mach1.microstep*9.375)
 		return converted
 
-	def zaberMove(command = None, data = None):
+	def zaberMove(self, command = None, data = None):
 		if command == None or data == None:
 			Exception("Method zaberMove must take inputs command, data.")
 
-	def setSpeed(v):
+	def setSpeed(self, v):
 		"""
 		v - speed to set in mm
+		converts to numbers that the stage wants and calls a command in the cmd dictionary
 		"""
-		converted = 0
-		return converted
+		converted = self.convertSpeed(v)
+		# set both stage speeds
+		self.zaberSend(Mach1.translation["hor"], self.cmd["setTargetSpeed"], data = converted)
+		self.zaberSend(Mach1.translation["ver"], self.cmd["setTargetSpeed"], data = converted)
 
-	def getSingleMeasurement(ch = "CH1"):
+	def zaberReceive(self):
+		# return 6 bytes from the receive buffer
+		# there must be 6 bytes to receive (no error checking)
+		r = [0,0,0,0,0,0]
+		for i in range (6):
+			r[i] = ord(self.stage.read(1))
+			return r
+
+	def zaberSend(self, device, command, data=0):
+		"""
+		send a packet using the specified device number, command number, and data
+		The data argument is optional and defaults to zero
+		"""
+		packet = struct.pack('<BBl', device, command, data)
+		self.stage.write(packet)
+		r = self.zaberRecieve()
+		return r
+
+	def getSingleMeasurement(self, ch = "CH1"):
 		"""
 		ch - "CH1" or "CH2"
 			input which channel you want data from.
@@ -45,7 +75,7 @@ class Mach1():
 		# added try except block to take care of times when get_waveform() has a problem.
 		while True:
 			try:		
-				waveform = osc.get_waveform(source = ch, start = 0, stop = 0)
+				waveform = self.osc.get_waveform(source = ch, start = 0, stop = 0)
 				break
 			except:
 				print("Retry: " + str(counter))
